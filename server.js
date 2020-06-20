1dd3c83aa348ea36f50902fe61c3c2c5421b75ac
James Goff <james_goff88@outlook.com>
2020-06-20 18:51:14 -0500

    landing page update html

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 const path = require('path');
 var port = process.env.PORT || 5000;
 
 const app = express();
 
-//View  engine setup
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
 //Static folder
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
